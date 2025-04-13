e5b09e2aabc0299fb9851304df73de90eca5f8b4
sklbz <sokol.bozanic@tumo.world>
2025-04-13 19:57:18 +0200

    seems to work fine

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,20 @@ fn main() {
     let board = Board::init();
 
     for i in 0..64 {
-        print!("{} ", i);
+        let letter = match i % 8 {
+            0 => 'a',
+            1 => 'b',
+            2 => 'c',
+            3 => 'd',
+            4 => 'e',
+            5 => 'f',
+            6 => 'g',
+            7 => 'h',
+            _ => unreachable!(),
+        };
+        print!("{}{}\t", letter, i / 8 + 1);
         is_possible(&board, &(i, 0));
-        thread::sleep(Duration::from_millis(25));
+        thread::sleep(Duration::from_millis(250));
     }
 
     println!("{:b}", &board.get_bitboard(Color::White, Type::Rook));
