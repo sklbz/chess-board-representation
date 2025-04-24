cff217c66e37d4c0ec0c79eef354cbff2c4011d9
sklbz <sokol.bozanic@tumo.world>
2025-04-24 19:05:46 +0200

    knight movement seems to works, although some things seems weird

--- src/move/knight.rs
+++ src/move/knight.rs
@@ -25,21 +25,21 @@ pub fn knight_move_bitmask(knight_square: &Square, allies: &BitBoard) -> BitBoar
     }
 
     if knight_square.rank() < 1 {
-        mask &= !(knight_square.to_bitboard() >> (8 + 2));
-        mask &= !(knight_square.to_bitboard() >> (8 - 2));
+        //mask &= !(knight_square.to_bitboard() >> (8 + 2));
+        //mask &= !(knight_square.to_bitboard() >> (8 - 2));
     }
     if knight_square.rank() < 2 {
-        mask &= !(knight_square.to_bitboard() >> (7 + 8));
-        mask &= !(knight_square.to_bitboard() >> (9 + 8));
+        //mask &= !(knight_square.to_bitboard() >> (7 + 8));
+        //mask &= !(knight_square.to_bitboard() >> (9 + 8));
     }
 
     if knight_square.rank() > 5 {
-        mask &= !(knight_square.to_bitboard() << (7 + 8));
-        mask &= !(knight_square.to_bitboard() << (9 + 8));
+        //mask &= !(knight_square.to_bitboard() << (7 + 8));
+        //mask &= !(knight_square.to_bitboard() << (9 + 8));
     }
     if knight_square.rank() < 7 {
-        mask &= !(knight_square.to_bitboard() << (8 + 2));
-        mask &= !(knight_square.to_bitboard() << (8 - 2));
+        //mask &= !(knight_square.to_bitboard() << (8 + 2));
+        //mask &= !(knight_square.to_bitboard() << (8 - 2));
     }
 
     mask & !allies
