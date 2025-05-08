a88df9307f72bf1df91df50dcce8684590e664a2
sklbz <sokol.bozanic@tumo.world>
2025-05-08 10:06:53 +0200

    Solved a sign bug when castling

--- src/legal_moves/castle/castle_mask.rs
+++ src/legal_moves/castle/castle_mask.rs
@@ -2,27 +2,28 @@ use crate::{
     bitboard::{BitBoard, BitBoardGetter},
     board::Board,
     legal_moves::{
-        attack_mask::{self, generate_attack_mask, generate_attack_mask_single_square},
+        attack_mask::{generate_attack_mask, generate_attack_mask_single_square},
         misc::{Color, Square, Type},
     },
+    utils::{square_to_string, string_to_square},
 };
 
 pub fn castle_mask(board: &Board, color: Color) -> BitBoard {
     let home_square: Square = match color {
-        Color::White => 4,
-        Color::Black => 60,
+        Color::White => string_to_square("e1"),
+        Color::Black => string_to_square("e8"),
         _ => panic!("Null king does not castle"),
     };
 
     let rook_squares: Vec<Square> = match color {
-        Color::White => vec![0, 7],
-        Color::Black => vec![56, 63],
+        Color::White => vec![string_to_square("a1"), string_to_square("h1")],
+        Color::Black => vec![string_to_square("a8"), string_to_square("h8")],
         _ => panic!("Null king does not castle"),
     };
 
-    let king: Square = board
-        .get_bitboard(&color, &Type::King)
-        .get_occupied_squares()[0];
+    let king_bit: BitBoard = board.get_bitboard(&color, &Type::King);
+
+    let king: Square = king_bit.get_occupied_squares()[0];
     let rooks: Vec<Square> = board
         .get_bitboard(&color, &Type::Rook)
         .get_occupied_squares();
@@ -37,8 +38,6 @@ pub fn castle_mask(board: &Board, color: Color) -> BitBoard {
     }
 
     let mut castle_mask = 0;
-
-    let king_bit = 1 << king;
 
     for rook in valid_rooks.iter() {
         let mask: BitBoard = generate_attack_mask_single_square(board, rook, &(1 << king), &0);
@@ -56,7 +55,7 @@ pub fn castle_mask(board: &Board, color: Color) -> BitBoard {
             let attack_mask: BitBoard = generate_attack_mask(board, &!color, &0, &0);
 
             if attack_mask & travel_mask == 0 {
-                castle_mask |= 1 << (king - 2);
+                castle_mask |= 1 << (king + 2);
             }
 
             continue;
@@ -66,11 +65,12 @@ pub fn castle_mask(board: &Board, color: Color) -> BitBoard {
             continue;
         }
 
+        println!("5");
         let attack_mask: BitBoard = generate_attack_mask(board, &!color, &0, &0);
         let travel_mask: BitBoard = king_bit | king_bit >> 1 | king_bit >> 2;
 
         if attack_mask & travel_mask == 0 {
-            castle_mask |= 1 << (king + 2);
+            castle_mask |= 1 << (king - 2);
         }
     }
 
