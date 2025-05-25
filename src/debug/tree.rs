116f85288c257f56f328aa13d712ef456270b008
sklbz <sokol.bozanic@tumo.world>
2025-05-25 13:25:07 +0200

    Created new test function

--- src/debug/tree.rs
+++ src/debug/tree.rs
@@ -1,23 +1,68 @@
 use crate::{
     board::board::Board,
-    legal_moves::{generate_possible_moves::generate_move_vec, misc::Color},
+    legal_moves::{
+        generate_possible_moves::generate_move_vec,
+        misc::{Color, Move},
+    },
 };
 
-pub fn search_tree(board: &Board, color: Color, depth: usize) -> usize {
+pub struct SearchTree {
+    pub root: Vec<SearchTreeNode>,
+}
+
+pub struct SearchTreeNode {
+    pub move_: Move,
+    pub score: usize,
+    pub children: Vec<SearchTreeNode>,
+}
+
+pub fn search_tree_nodes(
+    board: &Board,
+    searched_move: Move,
+    color: Color,
+    depth: usize,
+) -> SearchTreeNode {
     if depth == 0 {
-        return generate_move_vec(board, color).len();
+        return SearchTreeNode {
+            move_: searched_move,
+            score: 1,
+            children: Vec::new(),
+        }; //generate_move_vec(board, color).len();
     }
 
-    let moves = generate_move_vec(board, color);
+    let moves: Vec<Move> = generate_move_vec(board, color);
 
-    let mut result = 0;
+    let mut children: Vec<SearchTreeNode> = Vec::new();
 
     for move_ in moves {
-        let mut next_board = board.clone();
-        next_board.play_move(&move_);
+        let child_board = board.as_played(&move_);
 
-        result += search_tree(&next_board, !color, depth - 1);
+        children.push(search_tree_nodes(&child_board, move_, !color, depth - 1));
     }
 
-    result
+    let score: usize = children.iter().fold(0, |acc, x| acc + x.score);
+
+    SearchTreeNode {
+        move_: searched_move,
+        score,
+        children,
+    }
+}
+
+pub fn search_tree(board: &Board, color: Color, depth: usize) -> SearchTree {
+    if depth == 0 {
+        panic!("Depth is 0");
+    }
+
+    let moves: Vec<Move> = generate_move_vec(board, color);
+
+    let mut children: Vec<SearchTreeNode> = Vec::new();
+
+    for move_ in moves {
+        let child_board = board.as_played(&move_);
+
+        children.push(search_tree_nodes(&child_board, move_, !color, depth - 1));
+    }
+
+    SearchTree { root: children }
 }
--- src/test/integration/mod.rs
+++ src/test/integration/mod.rs
@@ -5,4 +5,6 @@ pub mod divide;
 #[cfg(test)]
 pub mod perft;
 #[cfg(test)]
+pub mod search_tree;
+#[cfg(test)]
 pub mod test_games;
