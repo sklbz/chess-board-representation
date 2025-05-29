c94a2abf54580dfeafab0d757aca41b50efe2f12
sklbz <sokol.bozanic@tumo.world>
2025-05-29 13:55:48 +0200

    Problem found: pins do not work correctly

--- src/legal_moves/generate_possible_moves.rs
+++ src/legal_moves/generate_possible_moves.rs
@@ -1,12 +1,12 @@
 use crate::{
     bitboard::{BitBoard, BitBoardGetter},
     board::board::Board,
-    utils::mask_to_moves,
+    utils::{mask_to_moves, string_to_square},
 };
 
 use super::{
     castle::castle_mask::castle_mask,
-    /*helper_functions::{deflection_mask, is_pinned, is_pre_pinned, protection_mask}, */
+    helper_functions::{deflection_mask, is_pinned, is_pre_pinned, protection_mask},
     misc::{Color, Move, Square, Type},
     pseudo_legal_mask::generate_pseudo_move_mask,
 };
@@ -35,7 +35,7 @@ pub fn generate_move_mask(board: &Board, start: &Square) -> BitBoard {
         return generate_pseudo_move_mask(board, start) | castle_mask(board, color);
     }
 
-    let mut alt_board = board.clone();
+    /* let mut alt_board = board.clone();
 
     generate_pseudo_move_mask(board, start)
         .get_occupied_squares()
@@ -46,8 +46,7 @@ pub fn generate_move_mask(board: &Board, start: &Square) -> BitBoard {
             alt_board.play_move(&(**target, *start));
             !is_check
         })
-        .fold(0, |acc, target| acc | (1 << target))
-    /*
+        .fold(0, |acc, target| acc | (1 << target)) */
 
     let king_square = match board.get_bitboard(&color, &Type::King) {
         0 => return generate_pseudo_move_mask(board, start),
@@ -61,6 +60,15 @@ pub fn generate_move_mask(board: &Board, start: &Square) -> BitBoard {
     }
 
     let is_pinned = is_pinned(board, start, &king_square);
+
+    if start == &string_to_square("c3") {
+        println!(
+            "{} {} {}",
+            is_checked,
+            is_pinned,
+            is_pre_pinned(board, start, &king_square)
+        );
+    }
 
     if !is_checked && !is_pinned {
         return generate_pseudo_move_mask(board, start);
@@ -77,6 +85,7 @@ pub fn generate_move_mask(board: &Board, start: &Square) -> BitBoard {
            return 0;
        }
     */
+
     if is_checked && is_pinned {
         return 0;
     }
@@ -86,5 +95,4 @@ pub fn generate_move_mask(board: &Board, start: &Square) -> BitBoard {
     let deflection_mask: BitBoard = deflection_mask(is_checked, board, color);
 
     generate_pseudo_move_mask(board, start) & protection_mask & deflection_mask
-    */
 }
